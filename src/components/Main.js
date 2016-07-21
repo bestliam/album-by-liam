5261c4b2059430bbeb65b45bad74d3cf73e14c40
liam <9067173@qq.com>
2016-07-21 16:54:49 +0800

    组件分离

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,42 +1,41 @@
 require('normalize.css/normalize.css');
 require('styles/App.css');
-import ImgFigure from './ImgFigure';
+
 import React from 'react';
 
+import ImgFigure from './ImgFigure';
 
-/*
- * 主组件
- */
+//1、加载数据
+var imageDatas = require('../data/imageDatas.json');
+//2、添加相片连接到数据中，使用自执行函数
+/*imageDatas = (function getImgURL(imageDatasArr) {
+    for (let i = 0; i < imageDatasArr.length; i++){
+        let singleImageData = imageDatasArr[i];
+        singleImageData.imageURL = require('../images/' + singleImageData.filename);
+        imageDatasArr[i] = singleImageData;
+    }
+    return imageDatasArr;
+})(imageDatas);*/
+
 class AppComponent extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            imgsArrangeArr: [
-                /*{
-                 pos:{
-                 left: '0',
-                 top: '0'
-                 }
-                 rotate: 0,    //旋转角度
-                 isInverse: false,  //图片正反面
-                 isCenter: false,  //图片是否居中
-                 }*/
-            ]};
-    }
-
     render() {
-    return (
-      <section className="stage">
-          <section className="img-sec">
-              <ImgFigure />
-          </section>
-      </section>
-    );
-  }
+        var imgFigure = [];
+        imageDatas.forEach(function(value,index){
+            imgFigure.push(<ImgFigure data={value} key={index}/>);
+        });
+        return (
+            <section className="stage" ref="stage">
+                <section className="img-sec">
+                    {imgFigure}
+                </section>
+                <nav className="controller-nav"></nav>
+            </section>
+        )
+    }
 }
-
 AppComponent.defaultProps = {
+
 };
 
-export default AppComponent;
+export  default AppComponent;
